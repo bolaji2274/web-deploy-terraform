c793896c7588a39b3d4e366b5eb1cb184d61a484
bolaji2274 <hammedbolajihammed@gmail.com>
2025-10-09 08:54:09 +0100

    file modified

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
-resource "aws_instance" "web-deploy" {
-  ami = ""
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "web-deploy"
-  }
+resource "aws_vpc" "main" {
+  cidr_block = var.aws_vpc_cidr
 }
+
+# resource "aws_instance" "web-deploy" {
+#   ami = ""
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "web-deploy"
+#   }
+# }
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_secret_key" {
+}
+variable "aws_access_key" {
+}
+
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+variable "aws_vpc_cidr" {
+    default = "10.0.0.0/16"
+}
